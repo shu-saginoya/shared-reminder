e68ab39654635fa8a048c168b45df6aae67f9afd
shu-saginoya <saginoya@create-nippo.co.jp>
2022-06-10 13:45:48 +0900

    vue routerの設定

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,13 +10,23 @@ const router = createRouter({
       component: HomeView,
     },
     {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/Login.vue"),
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("../views/Register.vue"),
+    },
+    /*     {
       path: "/about",
       name: "about",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
-    },
+    }, */
   ],
 });
 
