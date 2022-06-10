d8adea4a0d00c5ae8b1a338babfb5bad7f2a9826
shu-saginoya <saginoya@create-nippo.co.jp>
2022-06-10 15:53:58 +0900

    VueAxiosのインストール

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,14 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import axios from "axios";
+import VueAxios from "vue-axios";
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueAxios, axios);
+app.provide("axios", app.config.globalProperties.axios);
 
 app.mount("#app");
